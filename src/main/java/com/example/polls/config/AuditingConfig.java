46d140e3d59ae07afcc515a7c786dac187f3aaef
VladiG1984 <vladig1984@gmail.com>
2018-09-16 23:55:17 +0300

    Added AuditingConfig

--- src/main/java/com/example/polls/config/AuditingConfig.java
+++ src/main/java/com/example/polls/config/AuditingConfig.java
@@ -1,10 +1,44 @@
 package com.example.polls.config;
 
+import com.example.polls.security.UserPrincipal;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
 
 @Configuration
 @EnableJpaAuditing
 public class AuditingConfig {
+
+	// To automatically populate the createdBy and updatedBy fields in UserDateAudit, we need to make the following modifications to the AuditingConfig class
 	
+	@Bean
+	public AuditorAware<Long> auditorProvider() {
+		return new SpringSecurityAuditAwareImpl();
+	}
+}
+
+class SpringSecurityAuditAwareImpl implements AuditorAware<Long> {
+	
+	
+	@Override
+	public Optional<Long> getCurrentAuditor() {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		
+		if (authentication == null ||
+				!authentication.isAuthenticated() ||
+				authentication instanceof AnonymousAuthenticationToken) {
+			return Optional.empty();
+		}
+		
+		UserPrincipal userPrincipal = (UserPrincipal) authentication.getPrincipal();
+		
+		
+		return Optional.ofNullable(userPrincipal.getId());
+	}
 }
